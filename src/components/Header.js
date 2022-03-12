b2586dc20e20a6524ebaa468e761a8aa1f39f10f
marksikaundi <emmanuelmark117@gmail.com>
2022-03-12 14:42:09 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
   return (
@@ -9,8 +10,17 @@ function Header() {
         </a>
 
         <Menu>
-
+          <a href="#">Model S</a>
+          <a href="#">Model 3</a>
+          <a href="#">Model X</a>
+          <a href="#">Model Y</a>
         </Menu>
+
+        <RightMenu>
+          <a href="#">Shop</a>
+          <a href="#">Account</a>
+          <CustomMenu />
+        </RightMenu>
     </Container>
   )
 }
@@ -19,13 +29,47 @@ export default Header
 
 const Container = styled.div`
   min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
 `
 
 const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+  
 
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+  @media(max-width: 768px) {
+    display: none;
+  }
 `
 
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+a {
+  font-weight: 600;
+  text-transform: uppercase;
+  margin-right: 10px;
+}
+`
 
+const CustomMenu = styled(MenuIcon)`
+   cursor: pointer; 
+`
 
 
 
